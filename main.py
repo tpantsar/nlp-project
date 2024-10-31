a282fb8acbbdc53a387be9f332ad8fb1fa2c5a4a
tpantsar <tomi.pantsar@gmail.com>
2024-10-31 12:07:39 +0200

    scale dataset values and import logger

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from dotenv import load_dotenv
 from scipy.stats import pearsonr
 from sklearn.preprocessing import MinMaxScaler
 
+from logger_config import logger
+
 # Load environment variables from .env file
 load_dotenv()
 
@@ -21,15 +23,18 @@ def get_search_count(query):
     response = requests.get(url, params=params)
 
     if response.status_code != 200:
-        print(f"Error: {response.json().get('error', {}).get('message')}")
+        logger.error(f"Error: {response.json().get('error', {}).get('message')}")
         return 0
 
     data = response.json()
+    logger.info(f"Response for query '{query}': {data}")
     # Check if search information is present in the response
     try:
         count = int(data["searchInformation"]["totalResults"])
-    except KeyError:
-        count = 0  # Handle cases with no results
+    except KeyError:  # Handle cases with no results
+        count = 0
+        logger.warning(f"No results found for query '{query}'")
+    logger.info(f"Result count for query '{query}': {count}")
     return count
 
 
@@ -51,23 +56,9 @@ def load_dataset(path: str) -> pd.DataFrame:
     """
     try:
         df = pd.read_csv(path, delimiter=";")
-        scaler = MinMaxScaler()
-
-        # Normalize the human scores between 0 and 1
-        df["human_score"] = scaler.fit_transform(
-            df["human_score"].values.reshape(-1, 1)
-        )
-
-        # Round the normalized scores to 2 decimal places
-        df["human_score"] = df["human_score"].round(2)
-
-        base, ext = os.path.splitext(path)
-        normalized_path = f"{base}_normalized{ext}"
-
-        # Save the updated DataFrame to a new CSV file
-        df.to_csv(normalized_path, index=False, sep=";")
+        logger.info(f"Loaded dataset from '{path}'")
     except FileNotFoundError:
-        print(f"Error: File not found at '{path}'")
+        logger.error(f"Error: File not found at '{path}'")
         return None
     return df
 
@@ -88,16 +79,26 @@ def compute_correlation(dataset_path):
         score = web_jaccard_similarity(P, Q)
         web_jaccard_scores.append(score)
 
+    logger.info(f"WebJaccard scores: {web_jaccard_scores}")
+    logger.info(f"Human scores: {human_scores}")
+
     # Compute Pearson correlation
     correlation, _ = pearsonr(web_jaccard_scores, human_scores)
     return correlation, web_jaccard_scores, human_scores
 
 
-# Example usage
-P = "laptop"
-Q = "computer"
-similarity_score = web_jaccard_similarity(P, Q)
-print(f"WebJaccard Similarity between '{P}' and '{Q}': {similarity_score}")
+def test():
+    # Example usage
+    P = "implement"
+    Q = "tool"
+    similarity_score = web_jaccard_similarity(P, Q)
+    result = f"WebJaccard Similarity between '{P}' and '{Q}': {similarity_score}"
+    logger.info(result)
+
+    # Save the similarity score to a file
+    with open("similarity_score.txt", "w") as file:
+        file.write(result)
+
 
 # Paths to datasets
 datasets = {
@@ -106,10 +107,50 @@ datasets = {
     "WordSim353": "datasets/wordsim.csv",
 }
 
-# Compute and print correlations for each dataset
-for name, path in datasets.items():
-    correlation, web_jaccard_scores, human_scores = compute_correlation(path)
-    if correlation is not None:
-        print(f"Pearson correlation for {name}: {correlation:.2f}")
-    else:
-        print(f"Error computing Pearson correlation for {name}")
+
+def normalize_human_scores():
+    """Normalize human scores in the DataFrame."""
+    for name, path in datasets.items():
+        df = load_dataset(path)
+
+        # Normalize the human scores between 0 and 1
+        scaler = MinMaxScaler(feature_range=(0, 1))
+
+        # Fit the scaler to the range [0, 10] and transform the values
+        df["human_score"] = scaler.fit([[0], [10]]).transform(
+            df["human_score"].values.reshape(-1, 1)
+        )
+
+        # Round the normalized scores to 3 decimal places
+        df["human_score"] = df["human_score"].round(3)
+
+        base, ext = os.path.splitext(path)
+        normalized_path = f"{base}_normalized{ext}"
+
+        # Save the updated DataFrame to a new CSV file
+        df.to_csv(normalized_path, index=False, sep=";")
+
+        if df is not None:
+            logger.info(f"Normalized '{name}' dataset to '{normalized_path}'")
+        else:
+            logger.error("Error normalizing human similarity scores")
+
+
+def calculate_correlations():
+    # Compute and print correlations for each dataset
+    for name, path in datasets.items():
+        correlation, web_jaccard_scores, human_scores = compute_correlation(path)
+        if correlation is not None:
+            logger.info(f"Pearson correlation for {name}: {correlation:.2f}")
+        else:
+            logger.error(f"Error computing Pearson correlation for {name}")
+
+
+def main():
+    normalize_human_scores()
+    # test()
+    # calculate_correlations()
+
+
+if __name__ == "__main__":
+    main()
